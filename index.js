a07ffe87b821766d108d423f9838548a7e0c559f
helloyou2012 <helloyou2012@yeah.net>
2017-10-09 10:22:07 +0800

    sequelize Op aliases

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 
 const orm = require('./lib/orm');
 
-module.exports = (db_configs) => {
+module.exports = (configs) => {
   // Object to Array
-  if (!(db_configs instanceof Array)) {
-    db_configs = [db_configs];
+  if (!(configs instanceof Array)) {
+    configs = [configs];
   }
 
-  const databases = orm(db_configs);
+  const databases = orm(configs);
 
   function db(name) {
-    const first = db_configs[0];
+    const first = configs[0];
     // Default is first database
     name = name || first.name || first.db || first.database;
     return databases[name];
